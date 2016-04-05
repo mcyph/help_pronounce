bf2db6403daad2218fb95eae421b0dae4f091f8e
joe bloggs <nobody@127.0.0.1>
2016-04-05 17:23:01 +1000

    initial commit

--- setup.py
+++ setup.py
@@ -3,19 +3,12 @@
 from setuptools import setup
 
 setup(
-    # GETTING-STARTED: set your app name:
-    name='YourAppName',
-    # GETTING-STARTED: set your app version:
+    name='EnunciatingPilchard',
     version='1.0',
-    # GETTING-STARTED: set your app description:
-    description='OpenShift App',
-    # GETTING-STARTED: set author name (your name):
-    author='Your Name',
-    # GETTING-STARTED: set author email (your email):
-    author_email='example@example.com',
-    # GETTING-STARTED: set author url (your url):
+    description='',
+    author='David Morrissey',
+    author_email='david.l.morrissey@gmail.com',
     url='http://www.python.org/sigs/distutils-sig/',
-    # GETTING-STARTED: define required django version:
     install_requires=[
         'Django==1.8.4'
     ],
