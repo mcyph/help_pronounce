f62ca5400b9bd814f0a2f255006386e3bcdf50dd
Dave Morrissey <20507948+mcyph@users.noreply.github.com>
2020-02-19 22:06:45 +1100

    disabled admin

--- help_pronounce/wsgi/enunciating/enunciating/urls.py
+++ help_pronounce/wsgi/enunciating/enunciating/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    #url(r'^admin/', admin.site.urls),
     url(r'^index$', views.index),
     url(r'^dynamic$', views.dynamic),
     url(r'^get_sound$', views.get_sound)
