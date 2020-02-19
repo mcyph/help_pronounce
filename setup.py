936b35826147a45dff1466cca46bbe9316e4c3d1
mcyph <20507948+mcyph@users.noreply.github.com>
2020-02-19 20:47:49 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup
 
 setup(
-    name='EnunciatingPilchard',
+    name='helppronounce',
     version='1.0',
     description='',
     author='David Morrissey',
-    author_email='david.l.morrissey@gmail.com',
+    author_email='20507948+mcyph@users.noreply.github.com',
     url='http://www.python.org/sigs/distutils-sig/',
     install_requires=[
-        'Django==1.8.4'
+        'Django' # ==1.8.4
     ],
     dependency_links=[
         'https://pypi.python.org/simple/django/'
