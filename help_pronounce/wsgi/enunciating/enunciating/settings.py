c2690eb32c21d9853a1e56ce4b248225e245fad4
Dave Morrissey <20507948+mcyph@users.noreply.github.com>
2020-02-20 16:51:21 +1100

    enabled debug for testing

--- help_pronounce/wsgi/enunciating/enunciating/settings.py
+++ help_pronounce/wsgi/enunciating/enunciating/settings.py
@@ -30,7 +30,7 @@ SECRETS = secrets.getter(os.path.join(DATA_DIR, 'secrets.json'))
 SECRET_KEY = SECRETS['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'openshift' not in os.getcwd() #os.environ.get('DEBUG') == 'True'
+DEBUG = True
 
 from socket import gethostname
 ALLOWED_HOSTS = [
